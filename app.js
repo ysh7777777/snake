6ee529c630f6d34e3f74d658973d0e70656488b7
ysh7777777 <yehshihfuaaa@gmail.com>
2026-05-27 14:15:39 +0800

    Ver1.0

--- app.js
+++ app.js
@@ -23,16 +23,47 @@ snake[3] = {
   x: 20,
   y: 0,
 };
+//食物位置
+class Food {
+  constructor() {
+    this.pickALocation();
+  }
+  pickALocation() {
+    this.x = Math.floor(Math.random() * column) * unit;
+    this.y = Math.floor(Math.random() * row) * unit;
+  }
 
+  //畫食物
+  drawFood() {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(this.x, this.y, unit, unit);
+  }
+}
+let myFood = new Food();
+//方向
+document.addEventListener("keydown", changeDirection);
 let d = "Right";
+function changeDirection(e) {
+  // console.log(e.key);
 
-function draw() {
-  console.log("正在執行");
-  //畫面刷新
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  if (e.key == "ArrowUp" && d != "Down") {
+    d = "Up";
+  } else if (e.key == "ArrowDown" && d != "Up") {
+    d = "Down";
+  } else if (e.key == "ArrowLeft" && d != "Right") {
+    d = "Left";
+  } else if (e.key == "ArrowRight" && d != "Left") {
+    d = "Right";
+  }
+}
 
-  //畫蛇
+let score = 0;
+let highestScore = Number(localStorage.getItem("highestScore")) || 0;
+document.getElementById("myScore").innerHTML = "分數:" + score;
+document.getElementById("myScore2").innerHTML = "最高分數:" + highestScore;
+
+//畫蛇
+function drawSnake() {
   for (let i = 0; i < snake.length; i++) {
     if (i == 0) {
       ctx.fillStyle = "lightgreen";
@@ -43,6 +74,15 @@ function draw() {
     ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
     ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
   }
+}
+function draw() {
+  console.log("正在執行");
+  //畫面刷新
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  drawSnake();
+  myFood.drawFood();
+
   //蛇動
   let HeadX = snake[0].x;
   let HeadY = snake[0].y;
@@ -66,33 +106,37 @@ function draw() {
     newHead.x = 0;
   }
   if (newHead.x < 0) {
-    newHead.x = canvas.width;
+    newHead.x = canvas.width - unit;
   }
   if (newHead.y >= canvas.height) {
     newHead.y = 0;
   }
   if (newHead.y < 0) {
-    newHead.y = canvas.height;
+    newHead.y = canvas.height - unit;
   }
 
-  //方向
-  document.addEventListener("keydown", changeDirection);
-  function changeDirection(e) {
-    console.log(e.key);
+  //碰到食物
+  if (newHead.x == myFood.x && newHead.y == myFood.y) {
+    myFood.pickALocation();
+    score++;
+    document.getElementById("myScore").innerHTML = "分數:" + score;
+  } else {
+    snake.pop();
+  }
+  if (score >= highestScore) {
+    highestScore = score;
+    localStorage.setItem("highestScore", highestScore);
+    document.getElementById("myScore2").innerHTML = "最高分數:" + highestScore;
+  }
 
-    if (e.key == "ArrowUp" && d != "Down") {
-      d = "Up";
-    } else if (e.key == "ArrowDown" && d != "Up") {
-      d = "Down";
-    } else if (e.key == "ArrowLeft" && d != "Right") {
-      d = "Left";
-    } else if (e.key == "ArrowRight" && d != "Left") {
-      d = "Right";
+  snake.unshift(newHead);
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(myGame);
+      alert("game over");
+      return;
     }
   }
-
-  snake.pop(); //123123
-  snake.unshift(newHead);
 }
 
 let myGame = setInterval(draw, 100);
